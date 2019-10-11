311eee0ad64b463c2e0534ee8dd651775d54dd99
dingsongyun <dingsongyun@xindong.com>
2019-10-11 21:38:58 +0800

    创建一个Windows窗体

---
+++ Source/Platform/Windows/HelloEngine_Win.c
@@ -0,0 +1,76 @@
+#include <windows.h>
+#include <windowsx.h>
+#include <tchar.h>
+
+LRESULT CALLBACK WindowProc(HWND hWnd,
+                         UINT message,
+                         WPARAM wParam,
+                         LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance,
+                   HINSTANCE hPrevInstance,
+                   LPTSTR lpCmdLine,
+                   int nCmdShow
+                   )
+{
+    HWND hWnd;
+
+    WNDCLASSEX wc;
+    ZeroMemory(&wc, sizeof(WNDCLASSEX));
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    wc.lpszClassName = _T("WindowClass1");
+
+    RegisterClassEx(&wc);
+
+    // Create Window
+    hWnd = CreateWindowEx(
+        0,
+        _T("WindowClass1"),
+        _T("Window Name"),
+        WS_OVERLAPPEDWINDOW,
+        300/* x-position */, 300/* y-position */,
+        500/* width */, 400/* Height */,
+        NULL,       // Window Parent
+        NULL,       // Window Menu
+        hInstance,  // Application handle
+        NULL        // used with multiple windows
+    );
+
+    ShowWindow(hWnd, nCmdShow);
+
+    MSG msg;
+    while(GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return msg.wParam;
+}
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    // sort through and find what code to run for the message given
+    switch(message)
+    {
+        // this message is read when the window is closed
+        case WM_DESTROY:
+            {
+                // close the application entirely
+                PostQuitMessage(0);
+                return 0;
+            } break;
+    }
+
+    // Handle any messages the switch statement didn't
+    return DefWindowProc (hWnd, message, wParam, lParam);
+}
+
+
+// Notice
+// Build Command : clang -l user32.lib -o HelloEngine_Win.exe HelloEngine_Win.c
